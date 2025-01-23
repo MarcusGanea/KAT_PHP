40b55940333d98f1dfac8c93cbc5dce8b7016962
MarcusGanea <ganea.marcus@gmail.com>
2025-01-23 15:07:17 +0100

    importet from bootstrap studio

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>KAT_PHP_FOREAL</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/line-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+</head>
+
+<body>
+    <div class="text-start" style="margin: 17px;padding: 0;">
+        <div class="text-end" style="height: 22px;"></div>
+        <div class="text-end"><button class="btn btn-primary" type="button" style="width: 160px;margin: 1px;"><i class="icon-settings"></i>Passwort ändern</button></div>
+        <div class="text-start" style="width: 250px;position: absolute;"><img src="assets/img/Knapp_Logo.png"></div>
+        <div class="text-end"><button class="btn btn-primary" type="button" style="width: 160px;margin: 1px;"> <i class="la la-info-circle"></i>Hilfestellung</button></div>
+        <div class="text-end"><button class="btn btn-primary" type="button" style="width: 160px;margin: 1px;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
+
+                    <path d="M17.4142 2.58579C16.6332 1.80474 15.3668 1.80474 14.5858 2.58579L7 10.1716V13H9.82842L17.4142 5.41421C18.1953 4.63316 18.1953 3.36683 17.4142 2.58579Z" fill="currentColor"></path>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M2 6C2 4.89543 2.89543 4 4 4H8C8.55228 4 9 4.44772 9 5C9 5.55228 8.55228 6 8 6H4V16H14V12C14 11.4477 14.4477 11 15 11C15.5523 11 16 11.4477 16 12V16C16 17.1046 15.1046 18 14 18H4C2.89543 18 2 17.1046 2 16V6Z" fill="currentColor"></path>
+                </svg>Fehler Melden</button></div>
+        <div class="text-end"><button class="btn btn-primary" type="button" style="width: 160px;margin: 1px;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em" height="1em" fill="currentColor">
+                    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
+                    <path d="M168 80c-13.3 0-24 10.7-24 24V408c0 8.4-1.4 16.5-4.1 24H440c13.3 0 24-10.7 24-24V104c0-13.3-10.7-24-24-24H168zM72 480c-39.8 0-72-32.2-72-72V112C0 98.7 10.7 88 24 88s24 10.7 24 24V408c0 13.3 10.7 24 24 24s24-10.7 24-24V104c0-39.8 32.2-72 72-72H440c39.8 0 72 32.2 72 72V408c0 39.8-32.2 72-72 72H72zM176 136c0-13.3 10.7-24 24-24h96c13.3 0 24 10.7 24 24v80c0 13.3-10.7 24-24 24H200c-13.3 0-24-10.7-24-24V136zm200-24h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H376c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H376c-13.3 0-24-10.7-24-24s10.7-24 24-24zM200 272H408c13.3 0 24 10.7 24 24s-10.7 24-24 24H200c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80H408c13.3 0 24 10.7 24 24s-10.7 24-24 24H200c-13.3 0-24-10.7-24-24s10.7-24 24-24z"></path>
+                </svg>ChangeLog</button></div>
+    </div>
+    <ul class="nav nav-tabs">
+        <li class="nav-item"><a class="nav-link active" href="#">Stammdaten</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Bewegungsdaten</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Auswertung</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Archivierung</a></li>
+    </ul>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
